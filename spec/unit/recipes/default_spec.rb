fe1e5dc87e8cb335aa0f6ff49f30112db6c7bb0e
Reece_Collett <rcollett@spartaglobal.com>
2018-09-26 19:27:37 +0100

    Unit tests written for code

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -18,18 +18,21 @@ describe 'nginx::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it "should install nginx" do
+      expect(chef_run).to install_package("nginx")
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it "should create a configuration template for reverse proxy" do
+      expect(chef_run).to create_template("/etc/nginx/sites-available/proxy.conf")
+    end
+
+    it "should create a symlink between sites-available and sites-enabled proxy.conf" do
+      expect(chef_run).to create_link("/etc/nginx/sites-enabled/proxy.conf").with_link_type(:symbolic)
+    end
+
+    it "should delete symlink between default" do
+      expect(chef_run).to delete_link("/etc/nginx/sites-enabled/default")
     end
   end
 end
